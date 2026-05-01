8dbfddd1b809214ed27c3e6b6a3bf47802edc9bb
soikat63 <soikat6389@gmail.com>
2026-05-01 17:55:51 +0600

    added filtered featured

--- src/app/all-photos/page.js
+++ src/app/all-photos/page.js
@@ -1,16 +1,28 @@
+import Category from "@/components/Category";
 import PhotoCard from "@/components/PhotoCard";
 
-const AllPhotosPage = async () => {
+const AllPhotosPage = async ({ searchParams }) => {
+  const { category } = await searchParams;
+  console.log(category);
+
   const res = await fetch("https://pixgen-dun.vercel.app/data.json");
   const photos = await res.json();
-//   console.log(photos);
+  //   console.log(photos);
+
+  const filteredPhotos = category
+    ? photos.filter(
+        (photo) => photo.category.toLowerCase() == category.toLowerCase(),
+      )
+    : photos;
 
   return (
     <div>
       <h2 className=" text-2xl font-bold m-4">AllPhotosPage</h2>
 
+      <Category />
+
       <div className=" grid grid-cols-4 gap-5">
-        {photos.map((photo) => (
+        {filteredPhotos.map((photo) => (
           <PhotoCard key={photo.id} photo={photo}></PhotoCard>
         ))}
       </div>
